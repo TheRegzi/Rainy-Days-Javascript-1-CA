84474a8853796096d77a6d1beb9252eb46716b15
Regine Kornbakk <142594277+TheRegzi@users.noreply.github.com>
2024-02-19 16:59:28 +0100

    fixed button link in js

--- js/checkout-success.js
+++ js/checkout-success.js
@@ -1,6 +1,10 @@
 function displayConfirmationMessage() {
     const confirmationMessage = document.getElementById("confirmation-message");
     confirmationMessage.innerHTML = `<img src="/images/checkmark.PNG" alt="Checkmark symbol" class="checkmark"><p>Thank you for your purchase!</p><p>We will send you an email that confirms your order.</p><p>Your tracking number is #001986313.</p><button class="continue-shopping">Continue Shopping</button>`
+    const continueShoppingButton = confirmationMessage.querySelector(".continue-shopping");
+    continueShoppingButton.addEventListener("click", () => {
+        window.location.href = "/index.html";
+    })
 }
 
 displayConfirmationMessage();
